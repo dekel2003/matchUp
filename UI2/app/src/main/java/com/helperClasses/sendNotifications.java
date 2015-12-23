91df604309b073320f26e366a31978879fcfc68a
Dekel Matalon <dekel2003@gmail.com>
2015-12-23 19:05:16 +0200

    small changes

---
+++ UI2/app/src/main/java/com/helperClasses/sendNotifications.java
@@ -0,0 +1,12 @@
+package com.helperClasses;
+
+import com.parse.ParseObject;
+
+/**
+ * Created by Dekel on 12/23/2015.
+ */
+public class sendNotifications {
+    public static void newMatch(String Alice_id, String Bob_id){
+        ParseObject po = 
+    }
+}
